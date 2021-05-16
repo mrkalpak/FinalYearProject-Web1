1a1b8309c0791406578d068554f77e37a94769a9
kalpak nemade <kalpaknemade3a@gmail.com>
2021-05-16 22:10:31 +0530

    .

--- admin/manageuser.php
+++ admin/manageuser.php
@@ -10,7 +10,7 @@
                 <th scope="col">Gender</th>
                 <th scope="col">Email</th>
                 <th scope="col">Contact No.</th>
-                <th scope="col"></th>
+                
 
             </tr>
         </thead>
@@ -52,7 +52,7 @@
                 snapshot.forEach(s => {
                     console.log(s)
                     const data = s.val();
-                    document.getElementById('content').innerHTML += "<tr><th scope='row'>" + i + "</th><td>" + data.name + "</td><td>" + data.gender + "</td><td>" + data.email + "</td><td>" + data.phone + "</td><td><a onclick='deleteUser(" + data.uId + ")'><svg xmlns='http://www.w3.org/2000/svg' width='20' height='20' fill='red' class='bi bi-trash' viewBox='0 0 16 16'><path d='M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z'/><path fill-rule='evenodd' d='M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z'/></svg></a></td></tr>"
+                    document.getElementById('content').innerHTML += "<tr><th scope='row'>" + i + "</th><td>" + data.name + "</td><td>" + data.gender + "</td><td>" + data.email + "</td><td>" + data.phone + "</td></tr>"
                     i++;
 
                 });
@@ -62,8 +62,6 @@
         })
 
     }
-    function deleteUser(uid){
-        
-    }
+     
 </script>
 <?php include "./footer.php"; ?>
